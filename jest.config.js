767df93633fd0a97852bb47b9ed541e5c80233df
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-18 10:24:11 -0300

    :lock: fix: resolve tests in ci

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     testEnvironment: 'jsdom',
-    testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+    testPathIgnorePatterns: ["/node_modules/", "/.next/", "<rootDir>/cypress/"],
     collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.test.ts(x)"],
+    collectCoverageFrom: ["<rootDir>/src/**/**/*.(test|spec).ts(x)"],
     setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
     moduleNameMapper: {
         "\\.(scss|css|sass)$": "identity-obj-proxy"
-    }
+    },
+    testMatch: ["<rootDir>/src/**/**/*.(test|spec).ts(x)"],
 }
