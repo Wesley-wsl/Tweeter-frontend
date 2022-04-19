c0da0e569c949a180ac7751129e2ed65a223d3e0
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-19 01:02:51 -0300

    :art: style: add variables root

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,6 +2,25 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
+:root {
+    --x-small: 1.4rem;
+    --small: 2rem;
+    --x-medium: 3rem;
+    --medium: 4rem;
+    --large: 4.8rem;
+    --blue-one: #2F80ED;
+    --blue-two: #2D9CDB;
+    --background-color: #F2F2F2;
+    --grey-one: #333333;
+    --grey-two: #4F4F4F;
+    --grey-three: #828282;
+    --grey-four: #BDBDBD;
+    --red-one:#EB5757 ;
+    --green-one: #27AE60;
+    --font-poppins: "Poppins" ;
+    --font-NotoSans: "Noto Sans" ;
+}
+
 * {
     margin: 0;
     padding: 0;
@@ -14,7 +33,7 @@ html {
 }
 
 body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: "Poppins", "Noto Sans", --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
 `;
