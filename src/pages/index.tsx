be09dc4cffd2dd2780d323dcf22e85ce10fd06fa
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-28 20:57:08 -0300

    :wrench: feat: create page for sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
-import Main from "../components/Main";
-import { NextSEO } from "../components/NextSEO";
+import CustomBackground from "../components/CustomBackground";
+import SignInForm from "../components/Form/SignInForm";
+import NextSEO from "../components/NextSEO";
 
-export default function Home() {
+export default function SignIn() {
     return (
-        <NextSEO title="Nextjs SEO - Boilerplate" description="A boilerplate.">
-            <Main />
+        <NextSEO
+            title="Tweeter - Sign In"
+            description="Sign In page for enter in tweeter with your account"
+        >
+            <CustomBackground image={"/background/background.webp"}>
+                <SignInForm />
+            </CustomBackground>
         </NextSEO>
     );
 }
