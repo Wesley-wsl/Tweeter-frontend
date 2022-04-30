3e6d395121294058d3d7802f0ca4c1b1b3075d58
Wesley Westelley <wesleywestelley@gmail.com>
2022-04-30 13:46:19 -0300

    :art: style: add stylization for home page

---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: var(--background-color);
+    min-height: 90vh;
+    margin: 2rem auto 7rem auto;
+    max-width: 110rem;
+    display: flex;
+`;
+
+export const Tweets = styled.section`
+    flex: 2;
+`;
+
+export const Aside = styled.section`
+    flex: 1;
+    margin-left: 2rem;
+`;
