915039bb0bef3c9ce42588a03d5436fb68cea9c1
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-01 17:44:53 -0300

    :boom: feat: configure toastify and add provider store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { DefaultSeo } from "next-seo";
 import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { ToastContainer, Zoom } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import SEO from "../../next-seo.config";
+import { store } from "../redux/store";
 import GlobalStyles from "../styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -9,7 +13,20 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <DefaultSeo {...SEO} />
             <GlobalStyles />
-            <Component {...pageProps} />
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                draggable={true}
+                pauseOnHover
+                transition={Zoom}
+                theme="light"
+            />
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
         </>
     );
 }
