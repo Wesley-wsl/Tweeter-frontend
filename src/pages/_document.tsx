2ca6b0eeca13896809705384a4b8411788e096d6
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-10 17:05:30 -0300

    :white_check_mark: feat: configure styled components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,18 +5,33 @@ import Document, {
     NextScript,
     DocumentContext,
 } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
 
-        ctx.renderPage = () =>
-            originalRenderPage({
-                enhanceApp: App => App,
-                enhanceComponent: Component => Component,
-            });
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: App => props =>
+                        sheet.collectStyles(<App {...props} />),
+                });
 
-        const initialProps = await Document.getInitialProps(ctx);
-        return initialProps;
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: [
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>,
+                ],
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 
     render() {
