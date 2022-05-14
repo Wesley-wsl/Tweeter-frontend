16f60a52d7c6cbfb1ccd804aaf2c0c406ed6dda7
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-14 17:59:48 -0300

    :recycle: refactor: remove redux

--- src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import userReducer from "./features/user";
-
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-    },
-});
