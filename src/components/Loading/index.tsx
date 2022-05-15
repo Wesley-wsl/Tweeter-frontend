8fd9fb05660de386373fb87cde040af49a0c5a56
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-15 00:30:10 -0300

    :white_check_mark: feat: create loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,5 @@
+import * as S from "./styles";
+
+export const Loading: React.FC = () => {
+    return <S.Loader />;
+};
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const Loader = styled.div`
+    background-color: var(--background-color-two);
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+
+    &:before {
+        transform: rotateX(60deg) rotateY(45deg) rotateZ(45deg);
+        animation: 750ms rotateBefore infinite linear reverse;
+    }
+
+    &:after {
+        transform: rotateX(240deg) rotateY(45deg) rotateZ(45deg);
+        animation: 750ms rotateAfter infinite linear;
+    }
+
+    &:before,
+    &:after {
+        box-sizing: border-box;
+        content: "";
+        display: block;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        margin-top: -7.5em;
+        margin-left: -7.5em;
+        width: 15em;
+        height: 15em;
+        transform-style: preserve-3d;
+        transform-origin: 50%;
+        transform: rotateY(50%);
+        perspective-origin: 50% 50%;
+        perspective: 340px;
+        background-size: 15em 15em;
+        background-image: url("/background/effectDark.svg");
+    }
+
+    @keyframes rotateBefore {
+        from {
+            transform: rotateX(60deg) rotateY(45deg) rotateZ(0deg);
+        }
+        to {
+            transform: rotateX(60deg) rotateY(45deg) rotateZ(-360deg);
+        }
+    }
+
+    @keyframes rotateAfter {
+        from {
+            transform: rotateX(240deg) rotateY(45deg) rotateZ(0deg);
+        }
+        to {
+            transform: rotateX(240deg) rotateY(45deg) rotateZ(360deg);
+        }
+    }
+`;
