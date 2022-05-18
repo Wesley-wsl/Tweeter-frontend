e51f05995bf57592599164d367d335d62cb69418
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-18 18:58:41 -0300

    :wrench: feat: fill all data about comment with real data

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,40 +1,40 @@
 import { Heart } from "@styled-icons/bootstrap";
 import Image from "next/image";
 
+import { ICommentData } from "../../@types";
 import * as S from "./styles";
 
-const Comment: React.FC = () => {
+export const Comment: React.FC<ICommentData> = ({ data }) => {
+    const createdAt = new Date(data.author.created_at).toLocaleDateString();
+
     return (
         <S.Container>
             <S.TopInformations>
                 <Image
                     width="45"
                     height="45"
-                    src="/background/akishino.webp"
+                    src={
+                        `http://localhost:3333/files/${data.author.avatar}` ??
+                        "https://pm1.narvii.com/6879/34a567bc12e59a4c20f723a0809f5ad9b6f1df2fr1-736-590v2_hq.jpg"
+                    }
                     alt="Profile Avatar"
                 />
                 <div>
-                    <p>Shino Aki</p>
-                    <p>24 August at 20:43</p>
+                    <p>{data.author.name}</p>
+                    <p>{createdAt}</p>
                 </div>
             </S.TopInformations>
 
             <S.Comment>
-                <p>
-                    I&apos;ve felt this pull many times, like while road
-                    tripping through Morocco. Seeking out the vastness of the
-                    desert, and looking inward at the same time.
-                </p>
+                <p>{data.comment}</p>
 
                 <div>
                     <p>
                         <Heart size={14} /> Liked
                     </p>
-                    <p>12k Likes</p>
+                    <p>{data.likes} Likes</p>
                 </div>
             </S.Comment>
         </S.Container>
     );
 };
-
-export default Comment;
