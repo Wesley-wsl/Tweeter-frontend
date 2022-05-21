fc0709ca01be27f6d9674675e5b32aded4893931
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-21 01:32:48 -0300

    :pencil2: chore: add types for tweet, author and comments

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -67,3 +67,69 @@ export interface IUser {
     avatar: string;
     id: string;
 }
+export interface ITweet {
+    id: string;
+    author_id: string;
+    likes: number;
+    liked_users_id: string[];
+    retweets_id: string[];
+    tweet_id: string | null;
+    image: string | null;
+    content: string;
+    comments_id: string[];
+    isPublic: string;
+    users_saved_id: string[];
+    author: IAuthor;
+    comments: IComment[];
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface IAuthor {
+    id: string;
+    name: string;
+    email: string;
+    avatar: string | null;
+    background: string | null;
+    followingCount: number;
+    followersCount: number;
+    about_me: string;
+    liked_tweets_id: string[];
+    liked_comments_id: string[];
+    retweets_id: string[];
+    bookmarks_id: string[];
+    created_at: Date;
+    updated_at: Date;
+}
+
+export interface IUserData {
+    data: {
+        data: IAuthor;
+    };
+    error: string;
+}
+
+export interface ITweetComponent {
+    data: ITweet;
+}
+
+export interface IComment {
+    id: string;
+    author_id: string;
+    author: IAuthor;
+    tweet_id: string;
+    tweet: ITweet;
+    comment: string;
+    likes: number;
+    liked_users_id: string[];
+    image?: string;
+    created_at: Date;
+}
+
+export interface ICommentData {
+    data: IComment;
+}
+
+export interface ILittleLoading {
+    color: string;
+}
