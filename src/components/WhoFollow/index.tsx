df46ae8a45940413f98750716855f69db8100c44
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-04 20:36:27 -0300

    :white_check_mark: feat: Show indication of who to follow.

--- src/components/WhoFollow/index.tsx
+++ src/components/WhoFollow/index.tsx
@@ -1,12 +1,23 @@
+import { useFetch } from "../../hooks/useFetch";
 import PersonToFollow from "../PersonToFollow";
 import * as S from "./styles";
 
 const WhoFollow: React.FC = () => {
+    const { data: whoFollow } = useFetch("/user/me/whofollow");
+
     return (
         <S.Container>
             <p>Who to follow</p>
             <S.WhoFollow>
-                <PersonToFollow />
+                {whoFollow && whoFollow.data[0] && (
+                    <PersonToFollow data={whoFollow.data[0]} />
+                )}
+                {whoFollow && whoFollow.data[1] && (
+                    <PersonToFollow data={whoFollow.data[1]} />
+                )}
+                {whoFollow && whoFollow.data.length === 0 && (
+                    <p>Nobody to indicate</p>
+                )}
             </S.WhoFollow>
         </S.Container>
     );
