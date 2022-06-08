4c881c97f78d8c3922c4e6345947cc762f5a803a
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-08 01:39:06 -0300

    :white_check_mark: feat: get tweets paginated for home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,46 @@
 import { GetServerSideProps } from "next";
+import { useEffect } from "react";
 
+import { ITweet } from "../../@types";
 import Header from "../../components/Header";
+import LittleLoading from "../../components/LittleLoading";
+import { Loading } from "../../components/Loading";
 import NextSEO from "../../components/NextSEO";
 import Trends from "../../components/Trends";
 import Tweet from "../../components/Tweet";
 import WhoFollow from "../../components/WhoFollow";
 import WriteTweet from "../../components/WriteTweet";
+import { useInfiniteScroll } from "../../hooks/useInfiniteScroll";
 import * as S from "../../styles/pages/Home";
 import { ensureAuthentication } from "../../utils/ensureAuthentication";
 
 export default function Home() {
+    const { isEndPage, ref, scrollLoading, tweets, handleFilter, setTweets } =
+        useInfiniteScroll(`/tweet`);
+
+    useEffect(() => {
+        handleFilter("latest");
+    }, []);
+
+    if (!tweets) return <Loading />;
+
     return (
         <NextSEO
             title="Tweeter - Home"
-            description="Homepage with tweets destinate to your profile"
+            description="Home page with tweets destinate to your profile"
         >
             <>
                 <Header />
                 <S.Container>
                     <S.Tweets>
-                        <WriteTweet />
-                        <Tweet />
+                        <WriteTweet setTweets={setTweets} />
+                        <div>
+                            {tweets.map((data: ITweet, index: number) => (
+                                <Tweet data={data} key={index} />
+                            ))}
+                            {!isEndPage && <div ref={ref} />}
+                            {scrollLoading && <LittleLoading color="#000" />}
+                        </div>
                     </S.Tweets>
 
                     <S.Aside>
