8e53ac81561e5fd2787ce71b963d13d3002f5e65
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-17 01:27:39 -0300

    :wrench: chore: create theme dark/light.

---
+++ src/styles/themes/index.ts
@@ -0,0 +1,91 @@
+const dark = {
+    background: {
+        primary: "#020202",
+        secondary: "#040404",
+        thirdy: "#000",
+        hover: "#070707",
+        input: "#080808",
+        container: "#020202",
+        textArea: "#070707",
+    },
+    text: {
+        primary: "#fff",
+        grey_100: "#f2f2f2",
+        grey_200: "#BDBDBD",
+        grey_300: "#E0E0E0",
+        grey_400: "#BDBDBD",
+        grey_500: "#E0E0E0",
+        red: "#EB5757",
+        green: "#27AE60",
+        header: "#fff",
+    },
+    button: {
+        primary: "#2F80ED",
+        secondary: "#2D9CDB",
+        boxShadow: "#000",
+        thirdy: "#0050bc",
+        hover: "#0069ff",
+    },
+    border: {
+        primary: "rgb(47, 51, 54)",
+    },
+    fonts: {
+        primary: "Poppins",
+        secondary: "Noto Sans",
+    },
+    svg: {
+        primary: "#bbb",
+        effect: "/background/effectLight",
+    },
+    scroll: {
+        track: "#020202",
+        thumb: "#080808",
+    },
+};
+
+const light = {
+    background: {
+        primary: "#f2f2f2",
+        secondary: "rgb(230, 230, 230)",
+        thirdy: "#fff",
+        hover: "rgb(230, 230, 230)",
+        input: "#f2f2f2",
+        container: "#fff",
+        textArea: "rgb(238, 238, 238)",
+    },
+    text: {
+        primary: "#000",
+        grey_100: "#333333",
+        grey_200: "#4F4F4F",
+        grey_300: "#828282",
+        grey_400: "#BDBDBD",
+        grey_500: "#E0E0E0",
+        red: "#EB5757",
+        green: "#27AE60",
+        header: "#4F4F4F",
+    },
+    button: {
+        primary: "#2F80ED",
+        secondary: "#2D9CDB",
+        boxShadow: "#2F80ED",
+        thirdy: "#2F80ED",
+        hover: "#2D9CDB",
+    },
+    border: {
+        primary: "rgba(0, 0, 0, 0.17)",
+    },
+    fonts: {
+        primary: "Poppins",
+        secondary: "Noto Sans",
+    },
+    svg: {
+        primary: "#111",
+        effect: "/background/effectLight",
+    },
+    scroll: {
+        track: "#f2f2f2",
+        thumb: "#BDBDBD",
+    },
+};
+
+export { dark, light };
