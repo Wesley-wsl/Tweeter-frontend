b999ec8ee8984f30199d2b378c5324f2e5ebff65
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-17 12:04:46 -0300

    :wrench: chore: create variants to modal animation

---
+++ src/utils/variants.ts
@@ -0,0 +1,32 @@
+export const modal = {
+    visible: {
+        opacity: 1,
+        scale: 1,
+        y: 0,
+    },
+    hidden: {
+        opacity: 0,
+        scale: 0,
+        y: "-100vh",
+    },
+};
+
+export const overlay = {
+    visible: {
+        opacity: 1,
+    },
+    hidden: {
+        opacity: 0,
+    },
+};
+
+export const widget = {
+    visible: {
+        opacity: 1,
+        scale: 1,
+    },
+    hidden: {
+        opacity: 0,
+        scale: 0,
+    },
+};
