d51183c5390f3d23c149c2ee42c76ebca8f8887c
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-17 10:31:00 -0300

    :wrench: feat: get users with infinite scroll

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,16 +1,17 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
-import { ITweet } from "../@types";
+import { IAuthor, ITweet } from "../@types";
 import api from "../services/api";
 
 export const useInfiniteScroll = (url: string) => {
     const [tweets, setTweets] = useState<ITweet[]>([]);
+    const [users, setUsers] = useState<IAuthor[]>([]);
     const [currentPage, setCurrentPage] = useState(0);
     const [isEndPage, setIsEndPage] = useState(false);
     const [scrollLoading, setScrollLoading] = useState(false);
     const [filter, setFilter] = useState("");
+    const [search, setSearch] = useState("");
     const { ref, inView } = useInView();
 
     function handleFilter(filterName: string) {
@@ -19,20 +20,34 @@ export const useInfiniteScroll = (url: string) => {
             setFilter(filterName);
             setIsEndPage(false);
             setTweets([]);
+            setUsers([]);
+        }
+    }
+
+    function handleSearch(e: FormEvent, searchName: string) {
+        e.preventDefault();
+        if (search !== searchName) {
+            setCurrentPage(0);
+            setSearch(searchName);
+            setIsEndPage(false);
+            setTweets([]);
+            setUsers([]);
         }
     }
 
     function handleReset() {
         setCurrentPage(0);
         setFilter("");
+        setSearch("");
         setIsEndPage(false);
         setTweets([]);
+        setUsers([]);
     }
 
     async function getTweets() {
         setScrollLoading(true);
         await api
-            .get(`${url}?page=${currentPage}&filter=${filter}`)
+            .get(`${url}?page=${currentPage}&filter=${filter}&search=${search}`)
             .then(response => {
                 setTweets((currentTweet: ITweet[]) => [
                     ...currentTweet,
@@ -44,19 +59,42 @@ export const useInfiniteScroll = (url: string) => {
         setScrollLoading(false);
     }
 
+    async function getUsers() {
+        setScrollLoading(true);
+        await api
+            .get(`/user?page=${currentPage}&name=${search}`)
+            .then(response => {
+                setUsers((currentUser: IAuthor[]) => [
+                    ...currentUser,
+                    ...response.data.data,
+                ]);
+                setCurrentPage(currentPage + 1);
+                setIsEndPage(response.data.lastPage);
+            });
+        setScrollLoading(false);
+    }
+
     useEffect(() => {
-        if (inView) {
-            getTweets();
-        }
-    }, [inView]);
+        if (inView && filter !== "people") getTweets();
+        if (inView && filter === "people") getUsers();
+    }, [inView, filter]);
+
+    useEffect(() => {
+        setSearch("");
+    }, [filter]);
 
     return {
         ref,
         scrollLoading,
         isEndPage,
         tweets,
+        users,
         handleFilter,
+        setUsers,
+        search,
         filter,
         handleReset,
+        handleSearch,
+        setTweets,
     };
 };
