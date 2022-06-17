ee40eb11eefc5b6a100f137bc1cc25aa0cb3d11d
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-17 13:51:50 -0300

    :white_check_mark: feat: add real data in trends

--- src/components/Trends/index.tsx
+++ src/components/Trends/index.tsx
@@ -1,33 +1,50 @@
+import { Close } from "@styled-icons/material";
 import React from "react";
 
+import { IShowTrend, ITrends } from "../../@types";
+import { useFetch } from "../../hooks/useFetch";
+import LittleLoading from "../LittleLoading";
 import * as S from "./styles";
 
-const Trends: React.FC = () => {
-    return (
-        <S.Container>
-            <p>Tends for you</p>
+const Trends = ({ handleSearch, search }: ITrends) => {
+    const { data: trends } = useFetch("/tweet/me/trends");
 
-            <div>
-                <p>#programming</p>
-                <p>213k Tweets</p>
-            </div>
-            <div>
-                <p>#devchallenges</p>
-                <p>123k Tweets</p>
-            </div>
-            <div>
-                <p>#frontend</p>
-                <p>34k Tweets</p>
-            </div>
-            <div>
-                <p>#backend</p>
-                <p>30k Tweets</p>
-            </div>
-            <div>
-                <p>#100DaysOfCode</p>
-                <p>5k Tweets</p>
-            </div>
-        </S.Container>
+    return (
+        <>
+            <S.Container>
+                <p>Trends for you</p>
+
+                {trends ? (
+                    trends.data.map((trend: IShowTrend, index: number) => (
+                        <div
+                            onClick={e =>
+                                trend.trend.slice(1) !== search
+                                    ? handleSearch(e, trend.trend.slice(1))
+                                    : handleSearch(e, "")
+                            }
+                            key={index}
+                            className={
+                                trend.trend.slice(1) === search
+                                    ? "trendActive"
+                                    : ""
+                            }
+                        >
+                            <p>{trend.trend}</p>
+                            <p>{trend.tweetsQuantity} Tweets</p>
+                            {trend.trend.slice(1) === search && (
+                                <S.CloseTrend>
+                                    <Close width={16} height={16} />
+                                </S.CloseTrend>
+                            )}
+                        </div>
+                    ))
+                ) : (
+                    <S.LoadingContainer>
+                        <LittleLoading color="#000" />
+                    </S.LoadingContainer>
+                )}
+            </S.Container>
+        </>
     );
 };
 
