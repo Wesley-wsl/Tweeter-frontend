ed93702e27b5ea0f94091ab358585a9da2ac1038
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-25 18:06:46 -0300

    :wrench: chore: configure coverage for tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
     testEnvironment: 'jsdom',
     testPathIgnorePatterns: ["/node_modules/", "/.next/", "<rootDir>/cypress/"],
     collectCoverage: true,
-    collectCoverageFrom: ["<rootDir>/src/**/**/*.(test|spec).ts(x)"],
+    collectCoverageFrom: ["<rootDir>/src/(components|pages)/**/*.ts(x)", "!**/*.stories.ts(x)", "!**/_app.tsx", "!**/_document.tsx"],
+    coverageReporters: ['lcov', 'text'],
     setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
     moduleNameMapper: {
         "\\.(scss|css|sass)$": "identity-obj-proxy"
