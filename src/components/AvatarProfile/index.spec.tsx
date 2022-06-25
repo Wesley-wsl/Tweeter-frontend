2d12d73adfb30105d13524a4b3cd1586b1d53cc0
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-25 16:50:26 -0300

    :test_tube: feat: ensure that avatar profile work correctly

---
+++ src/components/AvatarProfile/index.spec.tsx
@@ -0,0 +1,34 @@
+import AvatarProfile from ".";
+import { fireEvent, render, screen } from "../../tests/mocks/setupProviders";
+
+describe("#AvatarProfile component.", () => {
+    test("Should render user's avatar profile.", () => {
+        render(<AvatarProfile avatar={null} userId="1" />);
+
+        const avatarProfile = screen.getByAltText("Profile Avatar");
+        expect(avatarProfile).toBeInTheDocument();
+    });
+
+    test("Should be able to open modal to edit avatar if user authenticated is own this profile.", () => {
+        render(<AvatarProfile avatar={null} userId="2" />);
+
+        const changeImage = screen.getByLabelText(
+            "A landscape icon to add an image.",
+        );
+        expect(changeImage).toBeInTheDocument();
+
+        fireEvent.click(changeImage);
+
+        const modal = screen.getByTestId("modal");
+        expect(modal).toBeInTheDocument();
+    });
+
+    test("Shouldn't be able to open modal to edit avatar if user authenticated don't is own this profile.", async () => {
+        render(<AvatarProfile avatar={null} userId="100" />);
+
+        const changeImage = screen.queryByLabelText(
+            "A landscape icon to add an image.",
+        );
+        expect(changeImage).toEqual(null);
+    });
+});
