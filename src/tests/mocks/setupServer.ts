4769abafd23764f45a568fbe3fed0504b73009d8
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-27 00:04:04 -0300

    :wrench: chore: update handlers

--- src/tests/mocks/setupServer.ts
+++ src/tests/mocks/setupServer.ts
@@ -1,20 +1,51 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-import { commentJest } from "./constants";
+import { userJest } from "./constants";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3333";
+export const baseURL =
+    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3333";
 
 const handlers = [
-    rest.put(`${baseURL}/comment/${commentJest.id}/like`, (req, res, ctx) => {
+    rest.put(`${baseURL}/comment/*/like`, (req, res, ctx) => {
         return res(ctx.status(200));
     }),
-    rest.delete(
-        `${baseURL}/comment/${commentJest.id}/like`,
-        (req, res, ctx) => {
-            return res(ctx.status(200));
-        },
-    ),
+    rest.delete(`${baseURL}/comment/*/like`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+    rest.put(`${baseURL}/user/*`, (req, res, ctx) => {
+        return res(
+            ctx.status(404),
+            ctx.json({
+                error: {
+                    response: {
+                        data: {
+                            error: "Access denied.",
+                        },
+                    },
+                },
+            }),
+        );
+    }),
+    rest.put(`${baseURL}/comment/1/like`, (req, res, ctx) => {
+        return res(ctx.status(500));
+    }),
+    rest.delete(`${baseURL}/comment/*/like`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+    rest.put(`/user/follower/*`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+    rest.delete(`/user/unfollow/*`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+    rest.get("/user/me/whofollow", (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                data: [{ ...userJest }, { ...userJest, name: "Second User" }],
+            }),
+        );
+    }),
 ];
-
 export const server = setupServer(...handlers);
