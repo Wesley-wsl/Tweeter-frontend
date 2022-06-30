9e7a573c83ecfba260da4feec789dd425190aee3
Wesley Westelley <wesleywestelley@gmail.com>
2022-06-30 00:21:46 -0300

    :wrench: chore: fix baseURL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const API_BASE_URL =
+    process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3333/api/v1";
 
 export const headerNav = [
     {
