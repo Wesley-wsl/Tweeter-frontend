bf14991c308cca51043e9e66309b67c91140c801
Wesley Westelley <wesleywestelley@gmail.com>
2022-07-13 03:04:24 -0300

    :wrench: chore: add baseUrl to storybook

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
 export const API_BASE_URL =
     process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3333/api/v1";
 
+export const BaseUrlStorybook =
+    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3333";
+
 export const headerNav = [
     {
         path: "/home",
