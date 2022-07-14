831979a2164ef2f782e83593b63ebfea757046d4
Wesley Westelley <wesleywestelley@gmail.com>
2022-07-14 01:32:28 -0300

    :test_tube: test: ensure that filter can't activated again

--- src/components/FilterTweets/index.spec.tsx
+++ src/components/FilterTweets/index.spec.tsx
@@ -9,6 +9,10 @@ import {
 
 const handleResetMocked = jest.fn();
 
+beforeEach(() => {
+    jest.resetAllMocks();
+});
+
 describe("#FilterTweets component.", () => {
     test("Should be able to render options for filter with a class active if is actived.", () => {
         render(
@@ -49,6 +53,31 @@ describe("#FilterTweets component.", () => {
         waitFor(() => {
             expect(screen.getByText("Latest")).toHaveClass("active");
             expect(screen.getByText("Tweets")).not.toHaveClass("active");
+        });
+    });
+
+    test("Shouldn't be able to active a filter if this filter already activated.", () => {
+        render(
+            <FilterTweets
+                options={[IFilterOptions.TWEETS, IFilterOptions.LATEST]}
+                filter="tweets"
+                handleReset={handleResetMocked}
+            />,
+        );
+
+        const tweetsOption = screen.getByText("Tweets");
+        const latestOption = screen.getByText("Latest");
+
+        expect(tweetsOption).toBeInTheDocument();
+        expect(latestOption).toBeInTheDocument();
+        expect(tweetsOption).toHaveClass("active");
+
+        fireEvent.click(tweetsOption);
+
+        expect(handleResetMocked).not.toHaveBeenCalled();
+        waitFor(() => {
+            expect(screen.getByText("Latest")).not.toHaveClass("active");
+            expect(screen.getByText("Tweets")).toHaveClass("active");
         });
     });
 });
