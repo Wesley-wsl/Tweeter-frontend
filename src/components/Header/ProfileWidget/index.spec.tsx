d207cd7351274388ddacb72c129096e8b600bd28
Wesley Westelley <wesleywestelley@gmail.com>
2022-07-14 01:35:13 -0300

    :recycle: refactor: change avatar to null

--- src/components/Header/ProfileWidget/index.spec.tsx
+++ src/components/Header/ProfileWidget/index.spec.tsx
@@ -24,7 +24,7 @@ describe("#ProfileWidget component.", () => {
                         user: {
                             id: "2",
                             name: "None",
-                            avatar: "null",
+                            avatar: null,
                         },
                     } as IAuthContext
                 }
